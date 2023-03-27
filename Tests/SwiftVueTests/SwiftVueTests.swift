b434517c6f13f3982b8e03956e2ae19721dd1805
Evan Kaneshige <evan.kaneshige@gmail.com>
2023-03-26 18:26:41 -0600

    Fixed a few errors

--- Tests/SwiftVueTests/SwiftVueTests.swift
+++ Tests/SwiftVueTests/SwiftVueTests.swift
@@ -2,10 +2,14 @@ import XCTest
 @testable import SwiftVue
 
 final class SwiftVueTests: XCTestCase {
+<<<<<<< HEAD
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         //XCTAssertEqual(SwiftVue().text, "Hello, World!")
     }
+=======
+    
+>>>>>>> da3ed20 (Fixed a few errors)
 }
