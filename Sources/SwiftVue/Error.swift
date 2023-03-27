ea1df44cd9230d0f8d59c73806b798f43054d2d7
Evan Kaneshige <evan.kaneshige@gmail.com>
2023-03-26 19:09:50 -0600

    Better error struct

--- Sources/SwiftVue/Error.swift
+++ Sources/SwiftVue/Error.swift
@@ -7,11 +7,28 @@
 
 import Foundation
 
-public enum SwiftVueError: Error {
+public enum SwiftVueError: LocalizedError {
     case networkError // Bad or no HTTP response
     case decodingError // Bad decoding of utf data from HTTP response
     case credentialError // Wrong credentials
     case xmlParsingError // XML parsing failed
     case urlError
     case unknown
+    
+    public var errorDescription: String? {
+        switch self {
+        case .networkError:
+            return "Networking Error"
+        case .decodingError:
+            return "Decoding Error"
+        case .credentialError:
+            return "Credential Error"
+        case .xmlParsingError:
+            return "XML Parsing Error"
+        case .urlError:
+            return "URL Error"
+        case .unknown:
+            return "Unknown Error"
+        }
+    }
 }
